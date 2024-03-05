0e3c45d411a26ebc9b26e7f5c28d4517ac3dc48a
Rafael Teruyuki <rafaelteruyuki08@gmail.com>
2024-03-05 09:55:41 -0300

    Remove js, add package.json

--- js/index.js
+++
@@ -1,7 +0,0 @@
-$(".profile").addClass("pre-enter");
-setTimeout(function(){
-	$(".profile").addClass("on-enter");
-}, 500);
-setTimeout(function(){
-	$(".profile").removeClass("pre-enter on-enter");
-}, 3000);
